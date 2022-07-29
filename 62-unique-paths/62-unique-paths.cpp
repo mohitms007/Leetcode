d7e85ccc29aeb6ae4068ee982d0176e8c7435fbc
Mohit Sharma <mohitms015@gmail.com>
2022-07-29 10:29:58 +0530

    Time: 3 ms (44.06%), Space: 6.4 MB (51.37%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    int findPaths(int m, int n, int i, int j) {
+//         if(i == m - 1 || j == n - 1) {
+//             return 1;
+//         }
+        
+//         if(i >= m || j >= n) {
+//             return 0;
+//         }
+        
+        vector<vector<int>> ans(m, vector<int> (n));
+        for(int i=m-1;i>= 0;i--){
+            for(int j=n-1;j >= 0;j--){
+                if(i == m - 1 || j == n - 1) {
+                    ans[i][j] = 1;
+                }else{
+                    ans[i][j] = ans[i + 1][j] + ans[i][j+1];
+                }
+            }
+        }
+      
+        
+        return ans[0][0];
+    }
+    
+    int uniquePaths(int m, int n) {
+        int i = 0;
+        int j = 0;
+        return findPaths(m, n, i , j);
+    }
+};
